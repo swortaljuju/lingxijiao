1cbdaa0aa0c8c09830fdbd679664b4fb4ededb53
Xinyi Zhang <xyzhang0914@gmail.com>
2020-08-11 04:12:59 +0000

    wip added support for new user and get posts

--- nsnss_server/app.js
+++ nsnss_server/app.js
@@ -38,6 +38,8 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello, world!').end();
 });
 
+// parse application/json
+app.use(express.bodyParser());
 
 let pool;
 app.use(async (req, res, next) => {
@@ -71,6 +73,48 @@ app.route('/tests/:city')
   });
 
 
+  app.route('/get/posts')
+    .get(async (req, res) => {
+      // [START cloud_sql_mysql_mysql_connection]
+      try {
+          // No pagination for now because I'm lazy.
+          const postStmt = 'SELECT * FROM posts';
+          // Pool.query automatically checks out, uses, and releases a connection
+          // back into the pool, ensuring it is always returned successfully.
+          postsQuery = pool.query(postStmt);
+
+          const commentStmt = 'SELECT * FROM comments';
+          commentsQuery = pool.query(commentStmt);
+
+          // Organize comments into field in each post.
+          const posts = await postQuery;
+          const comments = await commentsQuery;
+          const updatedPosts = posts.map(function(post) {
+            post["comments"] = comments.filter((comment) => {
+              comment.post_id == post.id;
+            })
+            return post;
+          });
+
+
+          // [END cloud_sql_mysql_mysql_connection]
+          res.status(200).json({data: updatedPosts}).end();
+
+      } catch (err) {
+          // If something goes wrong, handle the error in this section. This might
+          // involve retrying or adjusting parameters depending on the situation.
+          // [START_EXCLUDE]
+          logger.error(err);
+          return res
+          .status(500)
+          .json(
+              {message: 'Unable to successfully add user!'}
+          )
+          .end();
+          // [END_EXCLUDE]
+      }
+  });
+
 app.route('/add/:city-:country')
   .get(async (req, res) => {
     const city = req.params.city;
@@ -88,16 +132,45 @@ app.route('/add/:city-:country')
         logger.error(err);
         return res
         .status(500)
-        .send(
-            'Unable to successfully add city!'
+        .json(
+            {message: 'Unable to successfully add city!'}
         )
         .end();
         // [END_EXCLUDE]
     }
 
     // [END cloud_sql_mysql_mysql_connection]
-    res.status(200).send(`Successfully added for ${city} in ${country}`).end();
+    res.status(200).json({message: `Successfully added for ${city} in ${country}`}).end();
   });
+
+
+  app.route('/insert/user')
+  .post(async (req, res) => {
+    const params = req.body;
+    // [START cloud_sql_mysql_mysql_connection]
+    try {
+        const stmt = 'INSERT INTO users (nickname, avatar, contact_info, email) values (?, ?, ?, ?)';
+        // Pool.query automatically checks out, uses, and releases a connection
+        // back into the pool, ensuring it is always returned successfully.
+        await pool.query(stmt, [params.nickname, params.avatar, params.contact_info, params.email]);
+    } catch (err) {
+        // If something goes wrong, handle the error in this section. This might
+        // involve retrying or adjusting parameters depending on the situation.
+        // [START_EXCLUDE]
+        logger.error(err);
+        return res
+        .status(500)
+        .json(
+            {message: 'Unable to successfully add user!'}
+        )
+        .end();
+        // [END_EXCLUDE]
+    }
+
+    // [END cloud_sql_mysql_mysql_connection]
+    res.status(200).json({message: `Successfully added user ${params.email}`}).end();
+
+});
 
 
 
