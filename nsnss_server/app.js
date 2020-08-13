f17a5d1ab05ee4b7799116a7664ca83e1224af13
xy <xiaoyun.anye@gmail.com>
2020-08-12 21:28:48 -0700

    basic functionalities supported

--- nsnss_server/app.js
+++ nsnss_server/app.js
@@ -17,6 +17,8 @@
 // [START gae_node_request_example]
 const cors = require('cors');
 const express = require('express');
+const metadata = require('gcp-metadata');
+const {OAuth2Client} = require('google-auth-library');
 const bodyParser = require('body-parser');
 
 const poolPromise = require('./database');
@@ -31,13 +33,24 @@ const logger = winston.createLogger({
 });
 
 const app = express();
+const oAuth2Client = new OAuth2Client();
+
+// Cache externally fetched information for future invocations
+let aud;
+
+async function audience() {
+  if (!aud && (await metadata.isAvailable())) {
+    let project_number = await metadata.project('numeric-project-id');
+    let project_id = await metadata.project('project-id');
+
+    aud = '/projects/' + project_number + '/apps/' + project_id;
+  }
+
+  return aud;
+}
 
 app.use(cors());
 app.options('*', cors());
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
-});
 
 // parse application/json
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -58,6 +71,47 @@ app.use(async (req, res, next) => {
   }
 });
 
+
+async function validateAssertion(assertion) {
+  if (!assertion) {
+    return {};
+  }
+
+  // Check that the assertion's audience matches ours
+  const aud = await audience();
+
+  // Fetch the current certificates and verify the signature on the assertion
+  const response = await oAuth2Client.getIapPublicKeys();
+  const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
+    assertion,
+    response.pubkeys,
+    aud,
+    ['https://cloud.google.com/iap']
+  );
+  const payload = ticket.getPayload();
+
+  // Return the two relevant pieces of information
+  return {
+    email: payload.email,
+    sub: payload.sub,
+  };
+}
+
+
+app.get('/', async (req, res) => {
+  const assertion = req.header('X-Goog-IAP-JWT-Assertion');
+  let email = 'None';
+  try {
+    const info = await validateAssertion(assertion);
+    email = info.email;
+  } catch (error) {
+    console.log(error);
+  }
+  res
+    .status(200)
+    .send(`Hello ${email}`)
+    .end();
+});
 
 app.route('/tests/:city')
   .get(async (req, res) => {
@@ -125,7 +179,7 @@ app.route('/get/user_by_email/:user_email')
     // [START cloud_sql_mysql_mysql_connection]
     try {
         const stmt = `
-          SELECT * FROM users
+          SELECT id FROM users
           WHERE email = ?
           LIMIT 1
         `;
@@ -172,7 +226,15 @@ app.route('/get/faved_posts/:user_id')
     try {
         // No pagination for now because I'm lazy.
         const postStmt = `
-          SELECT posts.* FROM posts
+          SELECT 
+            posts.id,
+            posts.text,
+            posts.timestamp,
+            posts.pinned,
+            users.avatar,
+            users.nickname
+          FROM posts
+          INNER JOIN users ON posts.user_id = users.id
           INNER JOIN post_favs ON posts.id = post_favs.post_id
           WHERE posts.removed = FALSE AND post_favs.unfaved = FALSE AND post_favs.user_id = ?
         `;
@@ -181,9 +243,18 @@ app.route('/get/faved_posts/:user_id')
         const postsQuery = pool.query(postStmt, [req.params.user_id]);
 
         const commentStmt = `
-        SELECT comments.*, star_count FROM comments 
+        SELECT 
+          comments.id,
+          comments.text,
+          comments.timestamp,
+          comments.post_id,
+          users.avatar,
+          users.nickname,
+          COALESCE(star_count, 0) as star_count 
+        FROM comments 
+        INNER JOIN users ON comments.user_id = users.id
         INNER JOIN post_favs ON comments.post_id = post_favs.post_id
-        JOIN (
+        LEFT JOIN (
           SELECT comment_id, COUNT(*) as star_count
           FROM comment_likes
           WHERE unstared = FALSE
@@ -227,14 +298,33 @@ app.route('/get/posts')
     // [START cloud_sql_mysql_mysql_connection]
     try {
         // No pagination for now because I'm lazy.
-        const postStmt = 'SELECT * FROM `posts` WHERE removed = FALSE';
+        const postStmt = `
+        SELECT 
+          posts.id,
+          posts.text,
+          posts.timestamp,
+          posts.pinned,
+          users.avatar,
+          users.nickname
+        FROM posts
+        INNER JOIN users ON posts.user_id = users.id
+        WHERE posts.removed = FALSE`;
         // Pool.query automatically checks out, uses, and releases a connection
         // back into the pool, ensuring it is always returned successfully.
         const postsQuery = pool.query(postStmt);
 
         const commentStmt = `
-          SELECT comments.*, star_count FROM comments
-          JOIN (
+          SELECT 
+            comments.id,
+            comments.text,
+            comments.timestamp,
+            comments.post_id,
+            users.avatar,
+            users.nickname,
+            COALESCE(star_count, 0) as star_count 
+          FROM comments 
+          INNER JOIN users ON comments.user_id = users.id
+          LEFT JOIN (
             SELECT comment_id, COUNT(*) as star_count
             FROM comment_likes
             WHERE unstared = FALSE
@@ -357,6 +447,133 @@ app.route('/insert/post')
 
     // [END cloud_sql_mysql_mysql_connection]
     res.status(200).json({message: `Successfully added post ${params.text}`}).end();
+});
+
+
+app.route('/insert/comment')
+  .post(async (req, res) => {
+    const params = req.body;
+    // [START cloud_sql_mysql_mysql_connection]
+    try {
+        const stmt = 'INSERT INTO comments (user_id, text, timestamp, post_id) values (?, ?, NOW(), ?)';
+        // Pool.query automatically checks out, uses, and releases a connection
+        // back into the pool, ensuring it is always returned successfully.
+        await pool.query(stmt, [params.user_id, params.text, params.post_id]);
+    } catch (err) {
+        // If something goes wrong, handle the error in this section. This might
+        // involve retrying or adjusting parameters depending on the situation.
+        // [START_EXCLUDE]
+        logger.error(err);
+        return res
+        .status(500)
+        .json(
+            {message: 'Unable to successfully add comment!'}
+        )
+        .end();
+        // [END_EXCLUDE]
+    }
+
+    // [END cloud_sql_mysql_mysql_connection]
+    res.status(200).json({message: `Successfully added comment ${params.text}`}).end();
+});
+
+app.route('/insert/fav')
+  .post(async (req, res) => {
+    const params = req.body;
+    // [START cloud_sql_mysql_mysql_connection]
+    try {
+        const stmt = 'INSERT INTO post_favs (user_id, timestamp, post_id) values (?, NOW(), ?)';
+        // Pool.query automatically checks out, uses, and releases a connection
+        // back into the pool, ensuring it is always returned successfully.
+        await pool.query(stmt, [params.user_id, params.post_id]);
+    } catch (err) {
+        // If something goes wrong, handle the error in this section. This might
+        // involve retrying or adjusting parameters depending on the situation.
+        // [START_EXCLUDE]
+        logger.error(err);
+        return res
+        .status(500)
+        .json(
+            {message: 'Unable to successfully fav post!'}
+        )
+        .end();
+        // [END_EXCLUDE]
+    }
+
+    // [END cloud_sql_mysql_mysql_connection]
+    res.status(200).json({message: `Successfully faved post ${params.post_id}`}).end();
+});
+
+app.route('/insert/star')
+  .post(async (req, res) => {
+    const params = req.body;
+    // [START cloud_sql_mysql_mysql_connection]
+    try {
+        const stmt = 'INSERT INTO comment_likes (user_id, timestamp, comment_id) values (?, NOW(), ?)';
+        // Pool.query automatically checks out, uses, and releases a connection
+        // back into the pool, ensuring it is always returned successfully.
+        await pool.query(stmt, [params.user_id, params.comment_id]);
+    } catch (err) {
+        // If something goes wrong, handle the error in this section. This might
+        // involve retrying or adjusting parameters depending on the situation.
+        // [START_EXCLUDE]
+        logger.error(err);
+        return res
+        .status(500)
+        .json(
+            {message: 'Unable to successfully stared comment!'}
+        )
+        .end();
+        // [END_EXCLUDE]
+    }
+
+    // [END cloud_sql_mysql_mysql_connection]
+    res.status(200).json({message: `Successfully stared comment ${params.comment_id}`}).end();
+});
+
+app.route('/delete/:item_type')
+  .post(async (req, res) => {
+    const params = req.body;
+    // [START cloud_sql_mysql_mysql_connection]
+    try {
+        let stmt;
+        let key;
+        if (req.params.item_type == "post") {
+          stmt = 'UPDATE `posts` SET removed = TRUE, remove_timestamp = NOW() WHERE id = ?';
+          key = [params.id];
+        } else if (req.params.item_type == "comment") {
+          stmt = 'UPDATE `comments` SET removed = TRUE, remove_timestamp = NOW() WHERE id = ?';
+          key = [params.id];
+        } else if (req.params.item_type == "star") {
+          stmt = 'UPDATE `comment_likes` SET unstared = TRUE, unstar_timestamp = NOW() WHERE user_id = ? and comment_id = ?';
+          key = [params.user_id, params.comment_id];
+        } else if (req.params.item_type == "fav") {
+          stmt = 'UPDATE `post_favs` SET unfaved = TRUE, unfav_timestamp = NOW() WHERE user_id = ? and post_id = ?';
+          key = [params.user_id, params.post_id];
+        } else {
+          throw new Error(`Do not support deletion of ${req.params.item_type}.`)
+        }
+
+        // Pool.query automatically checks out, uses, and releases a connection
+        // back into the pool, ensuring it is always returned successfully.
+        await pool.query(stmt, key);
+
+    } catch (err) {
+        // If something goes wrong, handle the error in this section. This might
+        // involve retrying or adjusting parameters depending on the situation.
+        // [START_EXCLUDE]
+        logger.error(err);
+        return res
+        .status(500)
+        .json(
+            {message: `Unable to delete ${req.params.item_type}!`}
+        )
+        .end();
+        // [END_EXCLUDE]
+    }
+
+    // [END cloud_sql_mysql_mysql_connection]
+    res.status(200).json({message: `Successfully deleted ${req.params.item_type}!`}).end();
 });
 
 
